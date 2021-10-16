c0de84dc7015c52fe46e1a37399014c95ca3f60d
SajadVahmi <sajad.vahmi@gmail.com>
2021-10-16 15:41:51 +0330

    Aggregate root class created.

---
+++ Framework/Framework.Domain/Abstractions/AggregateRoot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Domain.Abstractions
+{
+    public abstract class AggregateRoot<TKey> : Entity<TKey>
+    {
+       
+        protected AggregateRoot() { }
+        protected AggregateRoot(TKey id) : base(id)
+        {
+        }
+       
+    }
+}
