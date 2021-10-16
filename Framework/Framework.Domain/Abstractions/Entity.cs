2f4b1583db04ac7b8ee604823b529de45385f608
SajadVahmi <sajad.vahmi@gmail.com>
2021-10-16 15:37:54 +0330

    Entity base class created.

---
+++ Framework/Framework.Domain/Abstractions/Entity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Domain.Abstractions
+{
+    public abstract class Entity<TKey> : IEquatable<Entity<TKey>>
+    {
+        public TKey Id { get; protected set; }
+        protected Entity() { }
+        protected Entity(TKey id)
+        {
+            Id = id;
+        }
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Entity<TKey>)obj);
+        }
+        public override int GetHashCode()
+        {
+            return EqualityComparer<TKey>.Default.GetHashCode(Id);
+        }
+        public bool Equals(Entity<TKey> other)
+        {
+            if (other == null) return false;
+            return this.Id.Equals(other.Id);
+        }
+    }
+}
