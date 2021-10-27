5bcb2d21735b97977f0d76b52309916de63806ea
SajadVahmi <sajad.vahmi@gmail.com>
2021-10-27 08:44:28 +0330

    prson id created.

---
+++ ValueObjectAsField/ValueObjectAsField/Domain/Models/PersonId.cs
@@ -0,0 +1,20 @@
+﻿using Framework.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueObjectAsField.Domain.Models
+{
+    public class PersonId : ValueObject<PersonId>
+    {
+        public PersonId(){ this.Value = Guid.NewGuid(); }
+        public PersonId(Guid value) { this.Value = value; }
+        public Guid Value { get; private set; }
+        protected override IEnumerable<object> GetAttributesToIncludeInEqualityCheck()
+        {
+            yield return Value;
+        }
+    }
+}
