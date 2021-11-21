6362d951cffcb4bee735ccb71f1a679b7a20f968
SajadVahmi <sajad.vahmi@gmail.com>
2021-11-21 17:58:34 +0330

    order and orderline completed

---
+++ ValueObjectAsField/ValueObjectAsField/Domain/Models/Order.cs
@@ -0,0 +1,45 @@
+﻿using Framework.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueObjectAsField.Domain.Models
+{
+    public class Order : AggregateRoot<OrderId>
+    {
+        public static Order Create=>new Order();
+
+        protected Order(){}
+        public Name Name { get; private set; }
+        public Address Address { get; private set; }
+
+        private List<OrderLine> orderLines;
+        public IReadOnlyList<OrderLine> OrderLines => orderLines.AsReadOnly();
+
+        public void SetAddress(string city, string street, int unit, string zipCode)
+        {
+            Address = Address.CreateWith(city, street, unit, zipCode);
+        }
+        public void SetName(string firstname, string lastname)
+        {
+            Name = Name.CreateWith(firstname, lastname);
+        }
+
+        public void AssignLines(List<OrderLine> orderLines)
+        {
+            this.orderLines = orderLines;
+        }
+
+        public void UpdateLines(List<OrderLine> orderLines)
+        {
+            var mustBeAdd = orderLines.Except(this.orderLines).ToList();
+            var mustRemove = this.orderLines.Except(orderLines).ToList();
+
+            mustBeAdd.ForEach(this.orderLines.Add);
+            mustRemove.ForEach(item => this.orderLines.Remove(item));
+        }
+
+    }
+}
---
+++ ValueObjectAsField/ValueObjectAsField/Domain/Models/OrderLine.cs
@@ -0,0 +1,34 @@
+﻿using Framework.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueObjectAsField.Domain.Models
+{
+    public class OrderLine : ValueObject<OrderLine>
+    {
+        public static OrderLine CreateWith(int productId, int quantity, long eachPrice) 
+            => new OrderLine(productId, quantity, eachPrice);
+        public int ProductId { get; private set; }
+        public int Quantity { get; private set; }
+        public long EachPrice { get; private set; }
+        public long Price => Quantity * EachPrice;
+        protected OrderLine() { }
+        protected OrderLine(int productId, int quantity, long eachPrice)
+        {
+            if(productId is default(int) || quantity is default(int)) 
+                throw new ArgumentException("The productId and quantity are required.");
+            ProductId = productId;
+            Quantity = quantity;
+            EachPrice = eachPrice;
+        }
+        protected override IEnumerable<object> GetAttributesToIncludeInEqualityCheck()
+        {
+            yield return this.ProductId;
+            yield return this.Quantity;
+            yield return this.EachPrice;
+        }
+    }
+}
